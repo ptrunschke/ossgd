d77f6dc0dd2133daf1841ce03072baa9a5570ca1
Gruhlke <gruhlke@dirichlet.imp.fu-berlin.de>
2023-10-23 14:16:12 +0200

    exponential experiment

--- linear_sgd.py
+++ linear_sgd.py
@@ -6,6 +6,7 @@ import jax.numpy as jnp
 import numpy as np
 from numpy.polynomial.legendre import legval
 from fourier import fourval
+from exponential import compute_exponential_coefficients
 
 from tqdm import trange
 import matplotlib.pyplot as plt
@@ -14,6 +15,7 @@ descr = """Create a convergence plot for L2-SGD on a linear space."""
 parser = argparse.ArgumentParser(description=descr)
 parser.add_argument("-b", dest="basis", type=str, choices=["fourier", "legendre"], default="legendre", help="basis of the linear space")
 parser.add_argument("-d", dest="space_dimension", type=int, default=10, help="dimension of the linear space")
+parser.add_argument("-T", dest="target", type=str, choices=["random", "exp"], default="random", help="target function to use")
 parser.add_argument("-t", dest="target_dimension", type=int, default=10, help="dimension needed to represent the target function")
 #TODO: add a "stratified" sampling strategy
 parser.add_argument("-Z", dest="sampling_strategy", type=str, choices=["optimal", "boosted", "uniform"], default="optimal", help="sampling density to use")
@@ -28,10 +30,11 @@ args = parser.parse_args()
 try:
     args.step_size = float(args.step_size)
 except ValueError:
-    assert args.step_size in ["constant", "adaptive", "deterministic", "deterministic_unbounded"], args.step_size
+    assert args.step_size in ["constant", "adaptive", "deterministic", "deterministic_unbounded", "nouy"], args.step_size
 
 # xi_t = 1 / t^((1 + epsilon)/2)
 epsilon = 0.1
+# epsilon = 0.01
 # epsilon = 0.5 - 0.01
 assert 0 < epsilon < 0.5
 domain = (-1, 1)
@@ -40,7 +43,7 @@ loss_density = lambda x: jnp.full(x.shape, 0.5)
 
 os.makedirs("data", exist_ok=True)
 os.makedirs("plots", exist_ok=True)
-if args.step_size in ["constant", "adaptive", "deterministic", "deterministic_unbounded"]:
+if args.step_size in ["constant", "adaptive", "deterministic", "deterministic_unbounded", "nouy"]:
     step_size_str = args.step_size
 else:
     len_step_size_str = max(2, int(jnp.ceil(-jnp.log10(args.step_size))))
@@ -90,11 +93,15 @@ if __debug__:
 
 
 key = jax.random.PRNGKey(1)
-target_key, key = jax.random.split(key, 2)
-target_coefficients = jax.random.normal(key, shape=(args.target_dimension,))
-target_coefficients.at[:args.space_dimension].divide(jnp.linalg.norm(target_coefficients[:args.space_dimension]))
-target_coefficients.at[args.space_dimension:].divide(jnp.linalg.norm(target_coefficients[args.space_dimension:]))
-loss_gradient = lambda v: lambda x: evaluate_basis(x, v)  - evaluate_basis(x, target_coefficients)
+if args.target == "random":
+    target_key, key = jax.random.split(key, 2)
+    target_coefficients = jax.random.normal(key, shape=(args.target_dimension,))
+    target_coefficients.at[:args.space_dimension].divide(jnp.linalg.norm(target_coefficients[:args.space_dimension]))
+    target_coefficients.at[args.space_dimension:].divide(jnp.linalg.norm(target_coefficients[args.space_dimension:]))
+else:
+    assert args.target == "exp"
+    target_coefficients = compute_exponential_coefficients(degree=args.target_dimension-1)
+loss_gradient = lambda v: lambda x: evaluate_basis(x, v) - evaluate_basis(x, target_coefficients)
 minimal_loss = 0.5 * jnp.linalg.norm(target_coefficients[args.space_dimension:])**2
 print(f"Minimal loss: {minimal_loss:.2e}")
 error = lambda v: 0.5 * jnp.linalg.norm(v - target_coefficients[:args.space_dimension])**2
@@ -209,117 +216,13 @@ if args.step_size == "constant":
 elif args.step_size == "deterministic":
     def step_size(iteration, squared_gradient_norm):
         return maximal_step_size / iteration**(1 - epsilon)
+elif args.step_size == "nouy":
+    def step_size(iteration, squared_gradient_norm):
+        return 1. / iteration**0.8
 elif args.step_size == "deterministic_unbounded":
     def step_size(iteration, squared_gradient_norm):
         return 1 / iteration**(1 - epsilon)
 elif args.step_size == "adaptive":
-    # # ========= Compute the maximal step size =========
-    # L = 1
-    # C = 0
-    # μ = 1
-
-    # d = args.space_dimension
-    # n = args.sample_size
-
-    # κ = 1  # An upper bound for $\tilde\kappa$ in the neighborhood of the minimum.
-
-    # if args.sampling_strategy == "uniform":
-    #     if args.basis == "legendre":
-    #         K = 2*d + 1
-    #     else:
-    #         assert args.basis == "fourier"
-    #         K = d
-    # else:
-    #     assert args.sampling_strategy in ["optimal", "boosted"]
-    #     K = 1
-
-    # # We start by computing the maximal step size that allows for exponential convergence in the recovery setting.
-    # # We have the condition that
-    # # 0 < 1 - 2μ/κ (s - s^2 (L+C)/2 (1 + (Kd-1)/n)) <= c < 1 . Defining
-    # f = (L+C)/2 * (1 + (K*d-1)/n)
-    # k = 2*μ/κ
-    # # we can rewrite this condition as
-    # # -c < (1-c) - k (s - s^2 f) <= 0  ↔  -c < kf s^2 - k s + (1-c) <= 0. We now define
-    # p = -1/f
-    # # q = (1-c)/(kf)
-    # # and compute the step size bound via -p/2 ± sqrt(p^2/4 - q).
-    # # Note, however, that the choice of c is arbitrary and, ideally, we would like to choose c as small as possible.
-    # # The requirement p^2/4 - q >= 0 gives us the trivial condition
-    # # p^2/4 >= (1-c)/(kf)  ↔  kfp^2/4 >= 1-c  ↔  c >= 1 - kfp^2/4.
-    # # Taking the minimum possible c yields
-    # c = 1 - k * f * p**2/4
-    # print(f"Exponential decay factor: {c:.2e}")
-    # # We can now compute the step size bound via
-    # cc = 1 - c
-    # q = cc/(k*f)
-    # radicant = p**2/4 - q
-    # assert jnp.allclose(radicant, 0, atol=jnp.finfo(jnp.asarray(radicant).dtype).resolution)
-    # maximal_step_size = -p/2
-    # assert 0 < maximal_step_size
-    # print(f"Maximal step size: {maximal_step_size:.2e}")
-
-    # def exponential_convergence_factor(s, κ):
-    #     """c(κ)"""
-    #     # 1 - 2*μ/κ * (s - s^2 (L+C)/2 (1 + (Kd-1)/n)) == 1 - k * (s - s^2 f)
-    #     k = 2*μ/κ
-    #     # This is the factor that we would obtain with optimal (maximal) step size.
-    #     c = 1 - k * f * p**2/4
-    #     # This is the factor that we obtain with the selected step size.
-    #     # c = 1 - k * (s - s**2 * f)
-    #     assert jnp.all(jnp.isnan(c) | (c >= 0))
-    #     return c
-
-    # # Now we consider the conditions for descent in the general (non-recovery) setting.
-    # # First note, that for the gradient to provide descent, we require
-    # # s <= 1/(2h)
-    # # where h = (L+C)/2 (1 - 1/n) < f.
-    # # This means, that descent is already guaranteed by the maximal step size computed above.
-
-    # # Finally, we consider the conditions that arise due to the presence of a bias term.
-    # # Recall that we require
-    # # s(t)^2 * (L+C)/2 * d/n * |g(t)|^2 <= xi(t)
-    # # to be in ell^1 to ensure that a subsequence of the iterates converges to a stationary point.
-    # # By Stechkin's lemma, xi(t) has to decay asymptotically faster than 1/t.
-    # # Hence, we choose xi(t) = 1/t (although not in ell^1, it is the limit of all the ell^1 sequences t^{-1-ε} for ε→0).
-    # # Redefining the constant
-    # h = (L+C) * K * d / (2 * n)
-    h = (L+C) * V / (2 * n)
-    # # This yields the (asymptotic) upper bound
-    # # s(t)^2 * h * |g(t)|^2 = 1/t  →  s(t) = 1 / sqrt(t * h * |g(t)|^2) .
-
-    # Finally, we can combine all these conditions into a single adaptive step size rule.
-    def xi(iteration):
-        return 1 / iteration**(1 + epsilon)
-    assert xi(1) == 1
-    assert jnp.all(jnp.diff(xi(jnp.arange(1, 101))) < 0)
-
-    # step_size_factor = 1
-    # # Anthony's idea of scaling the step size sequence does not produce nice results.
-    # # To test this, just uncomment the following line.
-    # # step_size_factor = jnp.nan
-    # def step_size(iteration, squared_gradient_norm):
-    #     global step_size_factor
-    #     bias_step_size = jnp.sqrt(xi(iteration) / (h * squared_gradient_norm))
-    #     if jnp.isnan(step_size_factor):
-    #         step_size_factor = maximal_step_size / bias_step_size
-    #     bias_step_size *= step_size_factor
-    #     recovery_step_size = jnp.minimum(bias_step_size, maximal_step_size)
-    #     return recovery_step_size
-
-    # step_size_factor = 1
-    # # Anthony's idea of scaling the step size sequence does not produce nice results.
-    # # To test this, just uncomment the following line.
-    # # step_size_factor = jnp.nan
-    # def step_size(iteration, squared_gradient_norm):
-    #     global step_size_factor
-    #     if jnp.isnan(step_size_factor):
-    #         step_size_factor = jnp.sqrt(squared_gradient_norm)
-    #     xi_t = 1 / iteration**(0.5 + epsilon)
-    #     bias_step_size = xi_t / jnp.sqrt(squared_gradient_norm)
-    #     bias_step_size *= step_size_factor
-    #     recovery_step_size = jnp.minimum(bias_step_size, maximal_step_size)
-    #     return recovery_step_size
-
     step_size_factor = jnp.nan
     def step_size(iteration, squared_gradient_norm, indep_est):
         global step_size_factor
@@ -331,26 +234,6 @@ elif args.step_size == "adaptive":
         bias_step_size = s_t / jnp.sqrt(squared_gradient_norm)
         recovery_step_size = optimal_step_size * bias_step_size * jnp.sqrt(jnp.minimum(squared_gradient_norm, indep_est))
         return recovery_step_size
-
-    # There is no need to go through all this hustle.
-    # Half of the computation is not used in the end!
-
-    # step_size_factor = 1
-    # # Anthony's idea of scaling the step size sequence does not produce nice results.
-    # # To test this, just uncomment the following line.
-    # step_size_factor = jnp.nan
-    # def step_size(iteration, squared_gradient_norm):
-    #     global step_size_factor
-    #     if jnp.isnan(step_size_factor):
-    #         step_size_factor = jnp.sqrt(squared_gradient_norm)
-    #     # det_step_size = maximal_step_size / iteration**(0.5 + epsilon)
-    #     det_step_size = maximal_step_size / iteration**(1 - epsilon)
-    #     det_step_size *= 10
-    #     return min(step_size_factor * det_step_size / jnp.sqrt(squared_gradient_norm), maximal_step_size)
-    #     # bias_step_size *= step_size_factor
-    #     # recovery_step_size = jnp.minimum(bias_step_size, maximal_step_size)
-    #     # return recovery_step_size
-
 else:
     def step_size(iteration, _):
         return args.step_size
@@ -381,8 +264,8 @@ step_sizes[0] = np.nan
 for it in trange(1, args.iterations + 1, desc="Iteration"):
     squared_projected_gradient_norm = jnp.linalg.norm(target_coefficients[:args.space_dimension] - estimate)**2
     squared_gradient_norm = squared_projected_gradient_norm + jnp.linalg.norm(target_coefficients[args.space_dimension:])**2
-    assert jnp.allclose(squared_gradient_norm, loss_gradient(estimate)(xs)**2 * ps @ integral_weights, atol=1e-4, rtol=1e-3)
-    assert jnp.allclose(0.5 * loss_gradient(estimate)(xs)**2 * ps @ integral_weights, loss(estimate), atol=1e-4, rtol=1e-3)
+    # assert jnp.allclose(squared_gradient_norm, loss_gradient(estimate)(xs)**2 * ps @ integral_weights, atol=1e-4, rtol=1e-3)
+    # assert jnp.allclose(0.5 * loss_gradient(estimate)(xs)**2 * ps @ integral_weights, loss(estimate), atol=1e-4, rtol=1e-3)
     kappas[it] = squared_gradient_norm / squared_projected_gradient_norm
 
     sample_key, key = jax.random.split(key, 2)
